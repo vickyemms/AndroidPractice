1755e60a86c1d59f5b30177701dffdbb2ea214ea
vickyemms <64374691+vickyemms@users.noreply.github.com>
2023-02-28 18:48:14 +0100

    Add functionality

--- app/src/main/java/com/example/androidpractice/MainActivity.kt
+++ app/src/main/java/com/example/androidpractice/MainActivity.kt
@@ -2,15 +2,33 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
  class MainActivity : AppCompatActivity() {
 
+     private lateinit var etFirstNumber: EditText
+     private lateinit var etSecondNumber: EditText
+     private lateinit var btnAdd: Button
+     private lateinit var tvResult: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etFirstNumber = findViewById(R.id.etFirstNumber)
+        etSecondNumber = findViewById(R.id.etSecondNumber)
+        btnAdd = findViewById(R.id.btnAdd)
+        tvResult = findViewById(R.id.tvResult)
+
+        btnAdd.setOnClickListener{
+            var firstNumber = etFirstNumber.text.toString().toInt()
+            var secondNumber = etSecondNumber.text.toString().toInt()
+            var result = firstNumber + secondNumber
+            tvResult.text = result.toString()
+        }
+
     }
 
 }
