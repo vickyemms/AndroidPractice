d06395756f7513f20e27e6c2220c65813f871d73
vickyemms <64374691+vickyemms@users.noreply.github.com>
2023-03-01 18:38:25 +0100

    RadioButtons and CheckBox functionality

--- app/src/main/java/com/example/androidpractice/MainActivity.kt
+++ app/src/main/java/com/example/androidpractice/MainActivity.kt
@@ -3,22 +3,44 @@
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.ImageView
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
 
  class MainActivity : AppCompatActivity() {
 
-     private lateinit var btnImage: Button
-     private lateinit var ivImage: ImageView
+     private lateinit var rgMeat: RadioGroup
+     private lateinit var cbCheese: CheckBox
+     private lateinit var cbOnions: CheckBox
+     private lateinit var cbSalad: CheckBox
+     private lateinit var btnOrder: Button
+     private lateinit var tvOrder: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnImage = findViewById(R.id.btnImage)
-        ivImage = findViewById(R.id.ivImage)
+        rgMeat = findViewById(R.id.rgMeat)
+        cbCheese = findViewById(R.id.cbCheese)
+        cbOnions = findViewById(R.id.cbOnions)
+        cbSalad = findViewById(R.id.cbSalad)
+        btnOrder = findViewById(R.id.btnOrder)
+        tvOrder = findViewById(R.id.tvOrder)
 
-        btnImage.setOnClickListener{
-            ivImage.setImageResource(R.drawable.drawing)
+        btnOrder.setOnClickListener{
+            val checkedMeatRadioButtonId = rgMeat.checkedRadioButtonId
+            val meat = findViewById<RadioButton>(checkedMeatRadioButtonId)
+            val cheese = cbCheese.isChecked
+            val onions = cbOnions.isChecked
+            val salad = cbSalad.isChecked
+            val orderString = "You ordered a burger with:\n" +
+                    "${meat.text}" +
+                    (if(cheese) "\nCheese" else "") +
+                    (if(onions) "\nOnions" else "") +
+                    (if(salad) "\nSalad" else "")
+            tvOrder.text = orderString
         }
 
     }
