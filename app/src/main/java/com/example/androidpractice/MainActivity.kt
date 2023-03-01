817646e856211a93f55560b79033ad965ccfa32a
vickyemms <64374691+vickyemms@users.noreply.github.com>
2023-03-01 17:50:44 +0100

    Add Image functionality

--- app/src/main/java/com/example/androidpractice/MainActivity.kt
+++ app/src/main/java/com/example/androidpractice/MainActivity.kt
@@ -3,30 +3,22 @@
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.ImageView
 
  class MainActivity : AppCompatActivity() {
 
-     private lateinit var etFirstNumber: EditText
-     private lateinit var etSecondNumber: EditText
-     private lateinit var btnAdd: Button
-     private lateinit var tvResult: TextView
+     private lateinit var btnImage: Button
+     private lateinit var ivImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        etFirstNumber = findViewById(R.id.etFirstNumber)
-        etSecondNumber = findViewById(R.id.etSecondNumber)
-        btnAdd = findViewById(R.id.btnAdd)
-        tvResult = findViewById(R.id.tvResult)
+        btnImage = findViewById(R.id.btnImage)
+        ivImage = findViewById(R.id.ivImage)
 
-        btnAdd.setOnClickListener{
-            var firstNumber = etFirstNumber.text.toString().toInt()
-            var secondNumber = etSecondNumber.text.toString().toInt()
-            var result = firstNumber + secondNumber
-            tvResult.text = result.toString()
+        btnImage.setOnClickListener{
+            ivImage.setImageResource(R.drawable.drawing)
         }
 
     }
