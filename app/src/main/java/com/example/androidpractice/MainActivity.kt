21147a3dc9dd6b38b1e00a2bc966d8c15882315e
vickyemms <64374691+vickyemms@users.noreply.github.com>
2023-03-01 18:58:41 +0100

    Toast functionality

--- app/src/main/java/com/example/androidpractice/MainActivity.kt
+++ app/src/main/java/com/example/androidpractice/MainActivity.kt
@@ -3,44 +3,20 @@
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.CheckBox
-import android.widget.ImageView
-import android.widget.RadioButton
-import android.widget.RadioGroup
-import android.widget.TextView
+import android.widget.Toast
 
  class MainActivity : AppCompatActivity() {
 
-     private lateinit var rgMeat: RadioGroup
-     private lateinit var cbCheese: CheckBox
-     private lateinit var cbOnions: CheckBox
-     private lateinit var cbSalad: CheckBox
-     private lateinit var btnOrder: Button
-     private lateinit var tvOrder: TextView
+     private lateinit var btnToast: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        rgMeat = findViewById(R.id.rgMeat)
-        cbCheese = findViewById(R.id.cbCheese)
-        cbOnions = findViewById(R.id.cbOnions)
-        cbSalad = findViewById(R.id.cbSalad)
-        btnOrder = findViewById(R.id.btnOrder)
-        tvOrder = findViewById(R.id.tvOrder)
+        btnToast = findViewById(R.id.btnToast)
 
-        btnOrder.setOnClickListener{
-            val checkedMeatRadioButtonId = rgMeat.checkedRadioButtonId
-            val meat = findViewById<RadioButton>(checkedMeatRadioButtonId)
-            val cheese = cbCheese.isChecked
-            val onions = cbOnions.isChecked
-            val salad = cbSalad.isChecked
-            val orderString = "You ordered a burger with:\n" +
-                    "${meat.text}" +
-                    (if(cheese) "\nCheese" else "") +
-                    (if(onions) "\nOnions" else "") +
-                    (if(salad) "\nSalad" else "")
-            tvOrder.text = orderString
+        btnToast.setOnClickListener{
+            Toast.makeText(this, "I am a Toast", Toast.LENGTH_LONG).show()
         }
 
     }
