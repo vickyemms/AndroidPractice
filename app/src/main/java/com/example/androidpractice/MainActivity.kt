1bc1a05117efe48fc5e3a940d1fcddb4b92ef797
vickyemms <64374691+vickyemms@users.noreply.github.com>
2023-03-02 19:59:46 +0100

    Spinner

--- app/src/main/java/com/example/androidpractice/MainActivity.kt
+++ app/src/main/java/com/example/androidpractice/MainActivity.kt
@@ -2,31 +2,40 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.Toast
 
  class MainActivity : AppCompatActivity() {
 
+     private lateinit var spMonths: Spinner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        spMonths = findViewById(R.id.spMonths)
+
+        val customList = listOf("First", "Second", "Third", "Fourth")
+        val adapter = ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, customList)
+        spMonths.adapter = adapter
+
+        spMonths.onItemSelectedListener = object : OnItemSelectedListener {
+            override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                Toast.makeText(this@MainActivity,
+                    "You selected ${adapterView?.getItemAtPosition(position).toString()}",
+                    Toast.LENGTH_LONG).show()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+
+        }
+
     }
-
-     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-         menuInflater.inflate(R.menu.app_bar_menu, menu)
-         return true
-     }
-
-     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-         when(item.itemId){
-             R.id.miAddContact -> Toast.makeText(this, "You clicked on Add Contact", Toast.LENGTH_LONG).show()
-             R.id.miFavorites -> Toast.makeText(this, "You clicked on Favorites", Toast.LENGTH_LONG).show()
-             R.id.miSettings -> Toast.makeText(this, "You clicked on Settings", Toast.LENGTH_LONG).show()
-             R.id.miFeedback -> Toast.makeText(this, "You clicked on Give Feedback", Toast.LENGTH_LONG).show()
-             R.id.miClose -> finish()
-         }
-         return true
-     }
 
  }
