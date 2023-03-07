a9648551d2ecce38e3481742ddfa26bbd7ed0304
vickyemms <64374691+vickyemms@users.noreply.github.com>
2023-03-07 21:27:15 +0100

    Added JUnit tests

--- app/src/main/java/com/example/androidpractice/MainActivity.kt
+++ app/src/main/java/com/example/androidpractice/MainActivity.kt
@@ -13,71 +13,9 @@ import androidx.appcompat.app.AlertDialog
 
  class MainActivity : AppCompatActivity() {
 
-     private lateinit var btnDialog1: Button
-     private lateinit var btnDialog2: Button
-     private lateinit var btnDialog3: Button
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        btnDialog1 = findViewById(R.id.btnDialog1)
-        btnDialog2 = findViewById(R.id.btnDialog2)
-        btnDialog3 = findViewById(R.id.btnDialog3)
-
-        val addContactDialog = AlertDialog.Builder(this)
-            .setTitle("Add Contact")
-            .setMessage("Do you want to add Amanda to your contact list?")
-            .setIcon(R.drawable.ic_add_contact_foreground)
-            .setPositiveButton("Yes") {_, _ ->
-                Toast.makeText(this, "You have added Amanda to your contact list.", Toast.LENGTH_LONG).show()
-            }
-            .setNegativeButton("No") {_, _ ->
-                Toast.makeText(this, "You have not added Amanda to your contact list.", Toast.LENGTH_LONG).show()
-            }.create()
-
-        btnDialog1.setOnClickListener {
-            addContactDialog.show()
-        }
-
-        val options = arrayOf("First", "Second", "Third")
-        val singleChoiceDialog = AlertDialog.Builder(this)
-            .setTitle("Choose one of these options")
-            .setSingleChoiceItems(options, 0) { dialogInterface, i ->
-                Toast.makeText(this, "You clicked on ${options[i]}", Toast.LENGTH_SHORT).show()
-            }
-            .setPositiveButton("Accept") {_, _ ->
-                Toast.makeText(this, "You accepted your choice.", Toast.LENGTH_SHORT).show()
-            }
-            .setNegativeButton("No") {_, _ ->
-                Toast.makeText(this, "You declined your choice.", Toast.LENGTH_SHORT).show()
-            }.create()
-
-        btnDialog2.setOnClickListener {
-            singleChoiceDialog.show()
-        }
-
-        val multiChoiceDialog = AlertDialog.Builder(this)
-            .setTitle("Choose one of these options")
-            .setMultiChoiceItems(options, booleanArrayOf(false, false, false)) { dialogInterface, i, isChecked ->
-                if (isChecked){
-                    Toast.makeText(this, "You checked ${options[i]}", Toast.LENGTH_SHORT).show()
-                }else{
-                    Toast.makeText(this, "You unchecked on ${options[i]}", Toast.LENGTH_SHORT).show()
-                }
-            }
-            .setPositiveButton("Accept") {_, _ ->
-                Toast.makeText(this, "You accepted your choice.", Toast.LENGTH_SHORT).show()
-            }
-            .setNegativeButton("No") {_, _ ->
-                Toast.makeText(this, "You declined your choice.", Toast.LENGTH_SHORT).show()
-            }.create()
-
-        btnDialog3.setOnClickListener {
-            multiChoiceDialog.show()
-        }
-
 
     }
 
---
+++ app/src/test/java/com/example/androidpractice/RegistrationUtilTest.kt
@@ -0,0 +1,70 @@
+package com.example.androidpractice
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class RegistrationUtilTest{
+
+
+    @Test
+    fun `empty username returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "",
+            "123",
+            "123"
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `valid username and correctly repeated password returns true`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Philipp",
+            "123",
+            "123"
+        )
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun `username already exists returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Carl",
+            "123",
+            "123"
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `empty password returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Philipp",
+            "",
+            ""
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `incorrectly confirmed password returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Philipp",
+            "123",
+            "abc"
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `password contains less than 2 digits returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Philipp",
+            "abc1",
+            "abc1"
+        )
+        assertThat(result).isFalse()
+    }
+
+
+}
